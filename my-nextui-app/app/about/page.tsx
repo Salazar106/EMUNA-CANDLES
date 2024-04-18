3c20291d74590bd8f0437ecc00e54ecf51e28d3d
Daniel Peña <jdps106@hotmail.com>
2024-04-18 13:30:41 -0500

    se agregan textos

--- my-nextui-app/app/about/page.tsx
+++ my-nextui-app/app/about/page.tsx
@@ -9,8 +9,10 @@ export default function AboutPage() {
 			{/* <h1 className={title()}>About us</h1> */}
 			<div className="flex items-center justify-center gap-20 ">
 				<div>
-					<h1 className={title()}>About us</h1>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem architecto atque neque? Molestias eveniet ut, modi necessitatibus dolor quia vero voluptates quis qui impedit ullam delectus cupiditate illum reprehenderit libero.</p>
+					<h1 className={title()}>OUR HISTORY</h1>
+					<p className="mt-5">
+						Emuna Candles is more than just a brand; it's a story of courage, passion, and creativity that began with the Martinez family's journey from the lush landscapes of Colombia to the bustling city of Toronto, Canada. Inspired by the rich traditions of candle-making and a desire to infuse spaces with positive energy, Emuna was born in 2021.
+					</p>
 				</div>
 				<div className="relative">
 					<Image src="/image/Imagen1.png" alt="Ejemplo" width={900} height={400} />
@@ -22,18 +24,24 @@ export default function AboutPage() {
 					<Image src="/image/Imagen2.png" alt="Ejemplo" width={900} height={400} />
 				</div>
 				<div>
-					<h1 className={title()}>Mision</h1>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem architecto atque neque? Molestias eveniet ut, modi necessitatibus dolor quia vero voluptates quis qui impedit ullam delectus cupiditate illum reprehenderit libero.</p>
+					<h1 className={title()}>Why Choose Emuna?</h1>
+					<p>
+					Authenticity: Our candles are handcrafted with love and expertise, reflecting the heritage and dedication of our Colombian roots.
+Quality: We use only the finest beeswax and natural ingredients, ensuring a clean and long-lasting burn for your enjoyment.
+Energy Infusion: Each candle is designed to not only brighten your space but also to promote positivity and tranquility, creating a sanctuary wherever it is lit.
+Join the Emuna Family
+By choosing Emuna Candles, you are not just purchasing a product; you are embracing a story of resilience and innovation. Let our candles be a beacon of light and energy in your life's journey.
+					</p>
 				</div>
 			</div>
 
 			<div className="flex items-center justify-center gap-20 ">
 				<div>
-					<h1 className={title()}>Vision</h1>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem architecto atque neque? Molestias eveniet ut, modi necessitatibus dolor quia vero voluptates quis qui impedit ullam delectus cupiditate illum reprehenderit libero.</p>
+					<h1 className={title()}> OUR VISION</h1>
+					<p className="mt-5">At Emuna, we believe in the power of light to transform environments and uplift spirits. Our candles go beyond illumination; they are crafted with intention, combining pure beeswax and carefully selected essential oils to create a sensory experience that nurtures the soul.</p>
 				</div>
 				<div className="relative">
-					<Image src="/image/example.jpg" alt="Ejemplo" width={900} height={400} />
+					<Image src="/image/Imagen3.png" alt="Ejemplo" width={900} height={400} />
 				</div>
 			</div>
 		</div>
